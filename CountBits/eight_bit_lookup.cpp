2225d2a26e6027bea30b0f92519fc3398229b913
Anthony Foiani <anthony.foiani@gmail.com>
2013-09-14 15:36:17 -0600

    use the shared bit-counting function

--- CountBits/eight_bit_lookup.cpp
+++ CountBits/eight_bit_lookup.cpp
@@ -4,14 +4,7 @@ int eight_bit_lookup( const uint16_vec & vals )
 {
     int lookup[256];
     for ( uint16_t i = 0; i < 256; ++i )
-    {
-        uint16_t n = i;
-        n = ( ( n & 0xaaaa ) >> 1 ) + ( n & 0x5555 );
-        n = ( ( n & 0xcccc ) >> 2 ) + ( n & 0x3333 );
-        n = ( ( n & 0xf0f0 ) >> 4 ) + ( n & 0x0f0f );
-        n = ( ( n & 0xff00 ) >> 8 ) + ( n & 0x00ff );
-        lookup[i] = n;
-    }
+        lookup[i] = count_bits_uint16( i );
 
     int rv = 0;
     for ( std::size_t i = 0; i < vals.size(); ++i )
